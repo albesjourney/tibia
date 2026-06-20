9203ff6a61f5410d212dd013e391dc1b7a20bfc1
albesjourney <emnode@proton.me>
2026-06-20 03:17:34 +0200

    Added multiplayer support
    Players can now see each others on the map, and they can chat with each others.

--- server/src/world/message.rs
+++ server/src/world/message.rs
@@ -1,3 +1,5 @@
+use crate::chat::ChatType;
+use crate::map::position::Position;
 use crate::player::Player;
 use tokio::sync::mpsc::UnboundedSender;
 
@@ -5,7 +7,12 @@ use tokio::sync::mpsc::UnboundedSender;
 pub enum PlayerToWorld {
     Login(Player, UnboundedSender<WorldToPlayer>),
     Logout(Player),
+    UpdatePosition(Player),
+    Chat(Position, ChatType, Vec<u8>, String),
 }
 
-#[derive(Clone, Copy, Debug)]
-pub enum WorldToPlayer {}
+#[derive(Clone, Debug)]
+pub enum WorldToPlayer {
+    PlayerList(Vec<Player>),
+    Chat(Position, ChatType, Vec<u8>, String),
+}
