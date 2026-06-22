045720207b660e1c86b5f434ef8521598f308c69
albesjourney <emnode@proton.me>
2026-06-22 05:11:54 +0200

    Update mod.rs
    Major improvements to the server
    - Implemented chat modes (#B, #W, #Y, private message)
    - Moving objects
    - Using containers, including persistent map storage
    - Using equipment
    - Auto-walking using left click
    - Looking using left-and-right click
    - Added comments using rustdoc style
    
    Still need to improve using containers and moving items to/from them.

--- server/src/constants/mod.rs
+++ server/src/constants/mod.rs
@@ -1,9 +1,12 @@
+//! Shared constants and enumerations used across the server.
+
 use num_enum::TryFromPrimitive;
 
+/// Describes how a tile's object stack should be updated when sending map data to the client.
 #[repr(u8)]
 #[derive(Copy, Clone, Debug, Eq, PartialEq, TryFromPrimitive)]
 pub enum ObjectUpdateType {
     Remove = 0,
-    Add = 1,
+    Add    = 1,
     Update = 2,
 }
